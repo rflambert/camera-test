f8dc26bc66f7ca52011c8e037beb9f53563eb58e
Ronan Lambert <rflambert@live.co.uk>
2019-04-09 22:57:35 +1200

    General fixes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var constraints = { video: { facingMode: "environment" }, audio: false};
 
 const cameraView = document.querySelector('#camera--view'),
-cameraOutput = document.querySelector('#camera--output'),
 cameraSensor = document.querySelector('#camera--sensor'),
+cameraOutput = document.querySelector('#camera--output'),
 cameraTrigger = document.querySelector('#camera--trigger');
 
 // Access the device camera and stream to cameraView
